76bb15ccf1457994b9ee46af1a3e0a5f6112d962
Todd Wolfson <todd@twolfson.com>
2013-08-03 17:19:19 -0700

    Writing out tests

--- test/gittip-large-badges_test.js
+++ test/gittip-large-badges_test.js
@@ -1,11 +1,34 @@
-var gittip_large_badges = require('../lib/gittip-large-badges.js');
+// Load in dependencies
+var renderBadge = require('../lib/gittip-large-badges.js');
 
-describe('gittip-large-badges', function () {
+describe('A user', function () {
   before(function () {
-
+    this.username = 'twolfson';
   });
 
-  it('', function () {
+  describe('rendered into a badge', function () {
+    before(function (done) {
+      // Render the user
+      var that = this;
+      renderBadge(this.username, function (err, badgeStr) {
+        // Save the result
+        that.badgeStr = badgeStr;
 
+        // Callback with any errors
+        done(err);
+      });
+    });
+
+    it('generates a base 64 string', function () {
+      var badgeStr = this.badgeStr;
+      assert(badgeStr);
+      assert.strictEqual(typeof badgeStr, 'string');
+    });
+
+    describe('saved to an image', function () {
+      it('consistent with past images', function () {
+
+      });
+    });
   });
 });
